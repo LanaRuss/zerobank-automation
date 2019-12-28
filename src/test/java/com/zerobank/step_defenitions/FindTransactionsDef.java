be066e7f15efd181f6c6a5252a790c2d12dcf104
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2019-12-27 19:05:32 -0500

    just a few changes

--- src/test/java/com/zerobank/step_defenitions/FindTransactionsDef.java
+++ src/test/java/com/zerobank/step_defenitions/FindTransactionsDef.java
@@ -2,12 +2,22 @@ package com.zerobank.step_defenitions;
 
 import com.zerobank.pages.LoginPage;
 import io.cucumber.java.en.Given;
+import net.bytebuddy.implementation.bytecode.Throw;
+
+import java.util.concurrent.ExecutionException;
 
 public class FindTransactionsDef {
     LoginPage loginPage = new LoginPage();
 
     @Given("user is navigating to {string}")
     public void userIsNavigatingTo(String link) {
+        try {
         loginPage.navigateToModule(link);
+        }
+        catch (RuntimeException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException("Wrong module name");
+        }
     }
 }
--- src/test/resources/features/FindTransactions.feature
+++ src/test/resources/features/FindTransactions.feature
@@ -2,7 +2,7 @@ Feature: Find Transactions in Account Activity
 
 
   Scenario: Search date range
-  Given the user accesses the Find Transactions tab
+   Given the user accesses the Find Transactions tab
    When  the user enters date	range from "2012-09-01"	to "2012-09-06"
    And   clicks search
    Then  results table should only show transactions dates between "2012-09-01" to "2012-09-06"
